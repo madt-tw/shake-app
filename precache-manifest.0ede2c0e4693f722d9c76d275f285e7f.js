89c7d49434311bab933ae5c6d214d2916f674fac
Madison Tobal <mtobalo@thoughtworks.com>
2020-06-05 15:03:33 +0200

    Updates

--- precache-manifest.0ede2c0e4693f722d9c76d275f285e7f.js
+++ precache-manifest.0ede2c0e4693f722d9c76d275f285e7f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3627adad1840db8f9e856fb840e0c043",
+    "revision": "846a893f35c5fcfe04046628fea86547",
     "url": "/shake-app/index.html"
   },
   {
-    "revision": "372ce9cea0da155f33ce",
+    "revision": "d2bc1621f59c4ba0be0b",
     "url": "/shake-app/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "44e06e9cc059c48b5e44",
-    "url": "/shake-app/static/js/2.6627a0b4.chunk.js"
+    "revision": "f6ba246451045515dea1",
+    "url": "/shake-app/static/js/2.bee1d947.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/shake-app/static/js/2.6627a0b4.chunk.js.LICENSE.txt"
+    "url": "/shake-app/static/js/2.bee1d947.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "372ce9cea0da155f33ce",
-    "url": "/shake-app/static/js/main.7cbd0dc1.chunk.js"
+    "revision": "d2bc1621f59c4ba0be0b",
+    "url": "/shake-app/static/js/main.eb404a54.chunk.js"
   },
   {
     "revision": "d501ab63b66abe52b2db",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shake-app/precache-manifest.e7a7246882759dc7d3a63d814cabc4da.js"
+  "/shake-app/precache-manifest.0ede2c0e4693f722d9c76d275f285e7f.js"
 );
 
 self.addEventListener('message', (event) => {
